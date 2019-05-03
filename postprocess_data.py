ce6f40da819b124cb1cb1a14f937cfceaa1497a6
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2019-05-02 21:57:14 -0700

    clean script

--- postprocess_data.py
+++ postprocess_data.py
@@ -13,9 +13,6 @@
 #     name: altmetrics
 # ---
 
-# # Postprocessing
-# - merge researchers with phds/postdocs
-
 import pandas as pd
 from pathlib import Path
 
@@ -24,16 +21,18 @@ output_dir = Path("output/")
 researchers = pd.read_csv(output_dir / "researchers.csv", index_col="id", dtype={'ID_number':str})
 phds = pd.read_csv(output_dir / "phds.csv", index_col="id")
 postdocs = pd.read_csv(output_dir / "postdocs.csv", index_col="id")
-publications = pd.read_csv(output_dir / "publications.csv", index_col="id")
 
+# combine all multiple PhDs
 merged_phds = phds.groupby("rid")[['phd_institution','phd_institution_other','phd_year','phd_type']]
 merged_phds = merged_phds.apply(lambda x: x.apply(lambda y: " | ".join([str(_) for _ in y])))
 
+# combine all multiple postgrads
 merged_postdocs = postdocs.groupby("rid")[['postdoc_year', 'postdoc_status', 'postdoc_institution']]
 merged_postdocs = merged_postdocs.apply(lambda x: x.apply(lambda y: " | ".join([str(_) for _ in y])))
 
-a = researchers.merge(merged_phds, left_index=True, right_index=True, how="left")
-b = a.merge(merged_postdocs, left_index=True, right_index=True, how="left")
-b = b.rename(columns={'id':'rid'})
+# merge all dataframes
+researchers = researchers.merge(merged_phds, left_index=True, right_index=True, how="left")
+researchers = researchers.merge(merged_postdocs, left_index=True, right_index=True, how="left")
+researchers = researchers.rename(columns={'id':'rid'})
 
-b.to_csv("output/researchers_phds_postdocs.csv")
+researchers.to_csv(output_dir / "researchers_phds_postdocs.csv")
